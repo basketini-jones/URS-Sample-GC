345f537300ea407bd591af97d57a0e13a33794eb
basketini-jones <60999059+basketini-jones@users.noreply.github.com>
2022-03-29 11:57:04 -0400

    input stuff

---
+++ Assets/WebBrowserInputData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using Unity.WebRTC;
+using Unity.RenderStreaming;
+
+public class WebBrowserInputData : WebBrowserInputChannelReceiver
+{
+    RTCDataChannel channel;
+
+    public override void SetChannel(string connectionId, RTCDataChannel channel)
+    {
+        this.channel = channel;
+        base.SetChannel(connectionId, channel);
+        channel.OnMessage = bytes => {
+            string str = Encoding.Default.GetString(bytes);
+            Debug.Log(str);
+        };
+    }
+
+    public void SendData(string msg)
+    {
+        
+        channel.Send(msg);
+    }
+}
